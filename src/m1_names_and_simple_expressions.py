dd96423e7d99734870b3ed439fa4ee59d7a712d0
Matthew Doyle <mdoyle009@student.franciscan.edu>
2024-01-23 11:26:17 -0500

    02-m1 complete

--- src/m1_names_and_simple_expressions.py
+++ src/m1_names_and_simple_expressions.py
@@ -1,8 +1,23 @@
 answer = 2 + 5
 print(answer * 1000)
 
+solution = 77 - (13/2)
+print(solution)
+
+exponential = 5 ** 2
+print(exponential)
+
+result = 7 / 2
+print(result)
+
+quotient = 7 // 2
+print(quotient)
+
+remainder = 7 % 2
+print(remainder)
+
 ###############################################################################
-# TODO: 1. (1 pt)
+# DONE: 1. (1 pt)
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
 #     1. Computes 2 plus 5, yielding the object that is
 #          the integer 7.
@@ -26,7 +41,7 @@ print(answer * 1000)
 ###############################################################################
 
 ###############################################################################
-# TODO: 2. (1 pt)
+# DONE: 2. (1 pt)
 #   Immediately below this _TODO_, write code that:
 #     - Computes 77 minus the division between 13 and 2.
 #         - Do this in three ways:
@@ -45,7 +60,7 @@ print(answer * 1000)
 ###############################################################################
 
 ###############################################################################
-# TODO: 3. (1 pt)
+# DONE: 3. (1 pt)
 #   Immediately below this _TODO_, write code that:
 #     - Computes 5 to the 2nd power (HINT: use  **  as the operator)
 #     - Stores the result to a name of your choosing
@@ -54,7 +69,7 @@ print(answer * 1000)
 ###############################################################################
 
 ###############################################################################
-# TODO: 4. (1 pt)
+# DONE: 4. (1 pt)
 #   Immediately below this _TODO_, write code that:
 #     - Computes 7 divided by 2 and stores the result to the name  result  .
 #     - Computes just the quotient of 7 divided by 2 (HINT: use  //  as the 
