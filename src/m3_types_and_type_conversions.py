582d567e57b188972f8a5868a688ca2a45744383
Matthew Doyle <mdoyle009@student.franciscan.edu>
2024-01-23 11:59:43 -0500

    finished 02-m3

--- src/m3_types_and_type_conversions.py
+++ src/m3_types_and_type_conversions.py
@@ -1,7 +1,7 @@
 import math
 
 ###############################################################################
-# TODO: 1. (3 pts)
+# DONE: 1. (3 pts)
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number")
@@ -24,25 +24,39 @@ import math
 #      -- Write code of the form   print(type(BLAH)).
 #      -- RUN the code to LEARN its TYPE.
 #
-#       "hello"
-#       'hello'
-#       "a b c"
-#       3 + 3
-#       "3" + "3"
-#       2 ** 100
-#       2.0 ** 100
-#       math.sin(8)
-#       math.sin
-#       print
-#       math
-#       'math'
+#       "hello"     = str
+#       'hello'     = str
+#       "a b c"     = str
+#       3 + 3       = int
+#       "3" + "3"   = str
+#       2 ** 100    = int
+#       2.0 ** 100  = float
+#       math.sin(8) = float
+#       math.sin    = builtin_function_or_method
+#       print       = builtin_function_or_method
+#       math        = module
+#       'math'      = str
 #
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
 
+print(type(3.14))
+print(type("hello"))
+print(type('hello'))
+print(type("a b c"))
+print(type(3 + 3))
+print(type("3" + "3"))
+print(type(2 ** 100))
+print(type(2.0 ** 100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
+
 ###############################################################################
-# TODO: 2. (1 pt)
+# DONE: 2. (1 pt)
 #   As you can see from the first _TODO_, there is an important difference
 #   between a  float  type and an  integer  type. Floats can have decimals 
 #   while integer are just that, integers.
@@ -60,8 +74,12 @@ import math
 #   Once you have done this, change the above _TODO_ to DONE.
 ###############################################################################
 
+print(round(2.75))
+print(type(2.75))
+print(type(round(2.75)))
+
 ###############################################################################
-# TODO: 3. (1 pt)
+# DONE: 3. (1 pt)
 #   We can also manually convert an object from one type to another. This is 
 #   also called casting.
 #
@@ -80,3 +98,9 @@ import math
 #   
 #   Once you have done this, change the above _TODO_ to DONE.
 ###############################################################################
+
+value = 26.78
+print(value)
+print(type(value))
+print(int(value))
+print(type(int(value)))
